ab5b862e08029af2baea27ea7f2261a1a1a34a9e
tiheng0928 <tom830928@gmail.com>
2016-12-22 21:00:53 +0800

    add login

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express  = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
 
 app.set('view engine', 'ejs' );
 app.set('port', (process.env.PORT || 3000));
 
 //首頁
+
 app.get('/', function(req, res){
 	
 	res.render('pages/index.ejs');
@@ -19,7 +21,26 @@ app.get('/play', function(req, res){
 	res.render('pages/play');
 });
 
-var server = app.listen(app.get('port'), function(){
+//gaming頁
+app.get('/gaming', function(req, res){
+	//進入gaming頁
+	res.render('pages/gaming');
+});
+
+//socket
+io.on('connection', function(socket) {
+	console.log('socket connected');
+	socket.on('player entered', function(playername) {
+		socket.playername = playername;
+		console.log("New Player:"+playername.name+" Entered.");
+		//將player存入firbase
+		
+	});
+	
+});
+
+
+var server = server.listen(app.get('port'), function(){
 	console.log('Start server on port 3000:');
 });
 
