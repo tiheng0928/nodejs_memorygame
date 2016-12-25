2ca2e8ebe9726713fcd47041c6f74be3955af592
Iamjenius <amily_22036812@yahoo.com.tw>
2016-12-25 14:57:05 +0800

    遊戲大廳

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express  = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var room = {playernum: 0}
 
 
 app.set('view engine', 'ejs' );
@@ -21,20 +22,43 @@ app.get('/play', function(req, res){
 });
 
 //gaming頁
-app.get('/gaming', function(req, res){
+app.get('/gamelobby', function(req, res){
 	//進入gaming頁
-	res.render('pages/gaming');
+	res.render('pages/gamelobby.ejs');
 });
 
 //socket
 io.on('connection', function(socket) {
 	console.log('socket connected');
+	
 	socket.on('player entered', function(playername) {
 		socket.playername = playername;
 		console.log("New Player:"+playername.name+" Entered.");
 		//將player存入firbase
-		
 	});
+
+	
+	socket.on('player_num_plus',function() {
+		socket.room = room;
+		room.playernum++;
+		console.log(room.playernum);
+		io.sockets.emit('check_player_num',{playernum:room.playernum});
+		if (room.playernum == 4) {
+			io.sockets.emit('playable');
+		}
+	});
+	
+	socket.on('player_num_del',function(playernum) {
+		socket.room = room;
+		room.playernum--;
+		console.log(room.playernum);
+		io.sockets.emit('check_player_num',{playernum:room.playernum});
+		if (room.playernum ==! 4) {
+			io.sockets.emit('playdisable');
+		}
+	});
+
+
 	
 });
 
