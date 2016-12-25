1ef06eea97fde89477ade744bbdd0dc81663d33a
Iamjenius <amily_22036812@yahoo.com.tw>
2016-12-25 18:35:26 +0800

    限制人數完工囉!

--- app.js
+++ app.js
@@ -2,28 +2,28 @@ var express  = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-var room = {playernum: 0}
+var room = {playernum: 0, enter_id: 0}
 
 
 app.set('view engine', 'ejs' );
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static('public'));
-//­º­¶
+//Â­ÂºÂ­Â¶
 
 app.get('/', function(req, res){
 	
 	res.render('pages/index.ejs');
 });
 
-//play­¶
+//playÂ­Â¶
 app.get('/play', function(req, res){
-	//¶i¤Jplay­¶
+	//Â¶iÂ¤JplayÂ­Â¶
 	res.render('pages/play');
 });
 
-//gaming­¶
+//gamingÂ­Â¶
 app.get('/gamelobby', function(req, res){
-	//¶i¤Jgaming­¶
+	//Â¶iÂ¤JgamingÂ­Â¶
 	res.render('pages/gamelobby.ejs');
 });
 
@@ -34,34 +34,42 @@ io.on('connection', function(socket) {
 	socket.on('player entered', function(playername) {
 		socket.playername = playername;
 		console.log("New Player:"+playername.name+" Entered.");
-		//±Nplayer¦s¤Jfirbase
+		//Â±NplayerÂ¦sÂ¤Jfirbase
 	});
-
-	
+	socket.emit('set_enter_id',{enter_id:room.enter_id});
+	console.log(room.enter_id);
 	socket.on('player_num_plus',function() {
 		socket.room = room;
 		room.playernum++;
-		console.log(room.playernum);
+		room.enter_id = 1;
+		console.log(room.enter_id);
 		io.sockets.emit('check_player_num',{playernum:room.playernum});
-		if (room.playernum == 4) {
-			io.sockets.emit('playable');
+		socket.emit('set_enter_id',{enter_id:room.enter_id});
+		if(room.playernum == 4){
+			io.sockets.emit('full_result');
 		}
 	});
 	
 	socket.on('player_num_del',function(playernum) {
 		socket.room = room;
 		room.playernum--;
-		console.log(room.playernum);
+		room.enter_id = 0;
+		console.log(room.enter_id);
 		io.sockets.emit('check_player_num',{playernum:room.playernum});
-		if (room.playernum != 4) {
-			io.sockets.emit('playdisable');
+		socket.emit('set_enter_id',{enter_id:room.enter_id});
+		if(room.playernum != 4){
+			io.sockets.emit('empty_result');
 		}
 	});
-
-
-	
 });
 
+io.on('disconnect', function(socket) {
+	console.log('socket disconnected');
+	if(room.enter_id == 1){
+		room.playernum--;
+	}
+	console.log(room.playernum);
+});
 
 var server = server.listen(app.get('port'), function(){
 	console.log('Start server on port 3000:');
