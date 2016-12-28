4258c9dfc300cce19a4fe708690918e67eaa94bb
tiheng0928 <tom830928@gmail.com>
2016-12-28 20:37:52 +0800

    修改路由

--- app.js
+++ app.js
@@ -21,19 +21,19 @@ app.use(express.static('public'));
 
 app.get('/', function(req, res){
 	
-	res.render('pages/index.ejs');
+	res.render('pages/index');
 });
 
 //gaming頁面
 app.get('/gaming', function(req, res){
 	//進入gaming頁面
-	res.render('pages/gaming.ejs');
+	res.render('pages/gaming');
 });
 
 //遊戲大廳
 app.get('/gamelobby', function(req, res){
 	//進入遊戲大廳
-	res.render('pages/gamelobby.ejs');
+	res.render('pages/gamelobby');
 });
 
 //socket
