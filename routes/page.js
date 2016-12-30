f7693061cda7b6519039b594b909b290fc0aeef3
tiheng0928 <tom830928@gmail.com>
2016-12-30 11:38:42 +0800

    增加路由路徑

---
+++ routes/page.js
@@ -0,0 +1,8 @@
+exports.index = function (request, response) {
+    response.render('pages/index');
+};
+
+exports.gamelobby = function (request, response) {
+    response.render('pages/gamelobby');
+};
+
